85dafe7c2d6c93e62b7c7442c0d79d9f10c6a0e4
Ricardo Pérez <ricpelo@gmail.com>
2018-10-19 20:33:34 +0200

    Comprueba y conserva el valor del campo

--- index.php
+++ index.php
@@ -5,16 +5,16 @@
         <title>Tabla de multiplicar</title>
     </head>
     <body>
+        <?php $numero = isset($_GET['num']) ? trim($_GET['num']) : '' ?>
         <form action="" method="get">
             <label for="num">Número:</label>
-            <input id="num" type="text" name="num">
+            <input id="num" type="text" name="num" value="<?= $numero ?>">
             <input type="submit" value="Calcular">
         </form>
         <?php
         require './auxiliar.php';
 
-        if (isset($_GET['num'])) {
-            $numero = $_GET['num'];
+        if (!empty($numero)) {
             if (!ctype_digit($numero)) {
                 mostrarError("se ha pasado algo que no es un número.");
             } else {
