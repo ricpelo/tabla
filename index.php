384371d0cfef8561659708c363cee2235306dcf5
Ricardo Pérez <ricpelo@gmail.com>
2018-10-19 20:00:12 +0200

    Integra los dos archivos en uno

--- index.php
+++ index.php
@@ -5,12 +5,15 @@
         <title>Tabla de multiplicar</title>
     </head>
     <body>
+        <form action="" method="get">
+            <label for="num">Número:</label>
+            <input id="num" type="text" name="num">
+            <input type="submit" value="Calcular">
+        </form>
         <?php
         require './auxiliar.php';
 
-        if (!isset($_GET['num'])) {
-            mostrarError("falta el parámetro <i>num</i>.");
-        } else {
+        if (isset($_GET['num'])) {
             $numero = $_GET['num'];
             if (!ctype_digit($numero)) {
                 mostrarError("se ha pasado algo que no es un número.");
